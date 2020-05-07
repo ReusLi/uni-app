1e11cb5d17407269ff4317aed9bb3cf96d573c30
ReusLi <574399934@qq.com>
2020-05-07 15:06:45 +0800

    add code

---
+++ src/core/view/components/tsw-main-table/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<UniCollapse :accordion="false" tagname="UniCollapse" ref="sdk_uni_collapse_x3bc">
+		<UniCollapseItem title="主表信息" :disabled="false" :open="true" 
+		:showanimation="false">
+		<slot></slot>
+		</UniCollapseItem>
+	</UniCollapse>
+</template>
+
+<script>
+import uniCollapse from '../uni-collapse/index.vue';
+import uniCollapseItem from '../uni-collapse-item/index.vue';
+export default {
+	components: {
+		uniCollapse,
+		uniCollapseItem
+	},
+	name: 'SinglePageMain'
+};
+</script>
+
+<style></style>
